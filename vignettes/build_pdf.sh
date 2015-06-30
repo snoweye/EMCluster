a666fec2c44f45f7d78cf55993964070c55a9e54
wccsnow2014 <wccsnow2014@gmail.com>
2015-06-29 20:26:46 -0400

    Update authors

--- vignettes/build_pdf.sh
+++ vignettes/build_pdf.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-rm *.aux *.bbl *.blg *.log *.out *.toc
+R_HOME=`Rscript -e 'cat(R.home()[1])'`
+JSS_BST=${R_HOME}/share/texmf/bibtex/bst/jss.bst
+JSS_CLS=${R_HOME}/share/texmf/tex/latex/jss.cls
+
+rm *.aux *.bbl *.blg *.log *.out *.toc *.bst *.cls
+cp ${JSS_BST} ./
+cp ${JSS_CLS} ./
+
 pdflatex EMCluster-guide.Rnw
 bibtex EMCluster-guide
 pdflatex EMCluster-guide.Rnw
 pdflatex EMCluster-guide.Rnw
 pdflatex EMCluster-guide.Rnw
-rm *.aux *.bbl *.blg *.log *.out *.toc
 
+rm *.aux *.bbl *.blg *.log *.out *.toc *.bst *.cls
 mv -f *.pdf ../inst/doc/
 cp -f *.Rnw ../inst/doc/

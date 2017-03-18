fa0de120906bb98dca96f4c6d0f06c2bc10f6acd
wccsnow <wccsnow@gmail.com>
2017-03-18 12:09:04 -0400

    minor

--- R/fcn_ic.r
+++ R/fcn_ic.r
@@ -8,7 +8,7 @@ em.ic <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL,
     emobj <- list(n = nrow(x), pi = pi, Mu = Mu, LTSigma = LTSigma)
   }
   emobj$llhdval <- logL(x, emobj = emobj)
-  emobj$M <- length(emobj$pi) - 1 + length(emobj$Mu) + length(emobj$LTSigma)
+  emobj$adjM <- length(emobj$pi) - 1 + length(emobj$Mu) + length(emobj$LTSigma)
 
   ret <- list()
   ret$AIC <- em.aic(x, emobj = emobj)
@@ -24,14 +24,14 @@ em.aic <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL){
   if(is.null(emobj)){
     emobj <- list(pi = pi, Mu = Mu, LTSigma = LTSigma)
   }
-  if(is.null(emobj$M)){
-    emobj$M <- length(emobj$pi) - 1 + length(emobj$Mu) + length(emobj$LTSigma)
+  if(is.null(emobj$adjM)){
+    emobj$adjM <- length(emobj$pi) - 1 + length(emobj$Mu) + length(emobj$LTSigma)
   }
   if(is.null(emobj$llhdval)){
     emobj$llhdval <- logL(x, emobj = emobj)
   }
 
-  -2 * emobj$llhdval + 2 * emobj$M
+  -2 * emobj$llhdval + 2 * emobj$adjM
 }
 
 em.bic <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL){
@@ -41,14 +41,14 @@ em.bic <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL){
   if(is.null(emobj$n)){
     emobj$n <- nrow(x)
   }
-  if(is.null(emobj$M)){
-    emobj$M <- length(emobj$pi) - 1 + length(emobj$Mu) + length(emobj$LTSigma)
+  if(is.null(emobj$adjM)){
+    emobj$adjM <- length(emobj$pi) - 1 + length(emobj$Mu) + length(emobj$LTSigma)
   }
   if(is.null(emobj$llhdval)){
     emobj$llhdval <- logL(x, emobj = emobj)
   }
 
-  -2 * emobj$llhdval + log(emobj$n) * emobj$M
+  -2 * emobj$llhdval + log(emobj$n) * emobj$adjM
 }
 
 em.icl <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL){
@@ -58,8 +58,8 @@ em.icl <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL){
   if(is.null(emobj$n)){
     emobj$n <- nrow(x)
   }
-  if(is.null(emobj$M)){
-    emobj$M <- length(emobj$pi) - 1 + length(emobj$Mu) + length(emobj$LTSigma)
+  if(is.null(emobj$adjM)){
+    emobj$adjM <- length(emobj$pi) - 1 + length(emobj$Mu) + length(emobj$LTSigma)
   }
   if(is.null(emobj$llhdval)){
     emobj$llhdval <- logL(x, emobj = emobj)
@@ -68,7 +68,7 @@ em.icl <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL){
   Z.unnorm <- e.step(x, emobj = emobj, norm = FALSE)$Gamma
   logL.map <- do.call("c", lapply(1:emobj$n, function(i){ max(Z.unnorm[i, ]) }))
 
-  -2 * sum(logL.map) + log(emobj$n) * emobj$M
+  -2 * sum(logL.map) + log(emobj$n) * emobj$adjM
 }
 
 em.icl.bic <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL){
@@ -78,8 +78,8 @@ em.icl.bic <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL){
   if(is.null(emobj$n)){
     emobj$n <- nrow(x)
   }
-  if(is.null(emobj$M)){
-    emobj$M <- length(emobj$pi) - 1 + length(emobj$Mu) + length(emobj$LTSigma)
+  if(is.null(emobj$adjM)){
+    emobj$adjM <- length(emobj$pi) - 1 + length(emobj$Mu) + length(emobj$LTSigma)
   }
   if(is.null(emobj$llhdval)){
     emobj$llhdval <- logL(x, emobj = emobj)
@@ -90,7 +90,7 @@ em.icl.bic <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL){
   log.Z <- Z.unnorm - log(dmixmvn(x, emobj = emobj))
   logL.EN <- Z * log.Z
 
-  -2 * (emobj$llhdval + sum(logL.EN)) + log(emobj$n) * emobj$M
+  -2 * (emobj$llhdval + sum(logL.EN)) + log(emobj$n) * emobj$adjM
 }
 
 em.clc <- function(x, emobj = NULL, pi = NULL, Mu = NULL, LTSigma = NULL){
